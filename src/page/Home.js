623c5cdaa96f3d60d52205900b775f311d24526a
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-08-26 22:53:17 +0700

    Login Page

---
+++ src/page/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+
+function Home(){
+return(
+    <div>Hello World</div>
+
+)
+}
+
+export default Home;
---
+++ src/page/Login.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+function Login() {
+  return (
+    <>
+     <h1>เข้าสู่ระบบ</h1>
+     <h3>ลงชื่อเข้าใช้ด้วย KMUTT Accounts</h3>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="ชื่อผู้ใช้งาน"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "กรุณาใส่ชื่อผู้ใช้งาน",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="รหัสผ่าน"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "กรุณาใส่รหัสผ่าน",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+          <Button type="primary" htmlType="submit">
+            ลงชื่อเข้าใช้{" "}
+          </Button>{" "}
+      </Form>{" "}
+    </>
+  );
+}
+
+export default Login;
