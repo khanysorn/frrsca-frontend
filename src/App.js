b4b91b50d7aabc30ca34ed16a9d8cab08a2e380d
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-21 18:36:18 +0700

    [UPDATE] 🌎 Teacher Routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Home from './page/Home';
 import Login from './page/Login';
-import Class from './page/Class'
-import ClassDetail from './page/ClassDetail';
+import StudentClassList from './page/student/Class'
+import StudentClassDetail from './page/student/ClassDetail';
+import TeacherClassList from './page/teacher/Class'
+import TeacherClassDetail from './page/teacher/ClassDetail'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
@@ -13,8 +15,10 @@ function App() {
       <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
-      <Route path="/class" component={Class} />
-      <Route path="/classdetail" component={ClassDetail} />
+      <Route path="/student/class" component={StudentClassList} />
+      <Route path="/student/classdetail" component={StudentClassDetail} />
+      <Route path="/teacher/class" component={TeacherClassList} />
+      <Route path="/teacher/classdetail" component={TeacherClassDetail} />
       </Switch>
     </Router>
   );
