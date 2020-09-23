63aa7f44a55d6648a89bf9bcc008cdb0cb1c1ae7
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-23 13:14:04 +0700

    [UPDATE] Authentication Function

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import {Button} from "antd";
-// import firebase from "firebase";
-// import app from '../config';
+import firebase from "firebase";
+import app from '../config';
 import { useHistory } from "react-router-dom";
 import LoginBox from '../components/LoginBox'
 
 
-// const provider = new firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.OAuthProvider('microsoft.com');
 
 
 
@@ -17,29 +17,23 @@ function Login() {
 
   const [isLoading2,setLoading2] = useState(false);
 
-  // firebase.auth().getRedirectResult().then(function(result) {
-  //   if (result.credential) {
-  //     // This gives you a Google Access Token. You can use it to access the Google API.
-  //     const token = result.credential.accessToken;
-  //      history.push("/class");
-  //     // ...
-  //   }
-  //   // The signed-in user info.
-  //   const user = result.user;
-  // }).catch(function(error) {
-  //   // Handle Errors here.
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   const email = error.email;
-  //   // The firebase.auth.AuthCredential type that was used.
-  //   const credential = error.credential;
-  //   // ...
-  // });
+  firebase.auth().signInWithPopup(provider)
+  .then(function(result) {
+    // User is signed in.
+    // IdP data available in result.additionalUserInfo.profile.
+    // OAuth access token can also be retrieved:
+    // result.credential.accessToken
+    // OAuth ID token can also be retrieved:
+    // result.credential.idToken
+    history.push("/student/class")
+  })
+  .catch(function(error) {
+    // Handle error.
+  });
 
-  // function handlelogin(){
-  //   // app.auth().signInWithPopup(provider)
-  // }
+  function handlelogin(){
+    app.auth().signInWithPopup(provider)
+  }
 
   function mockloginstudent(){
     setLoading1(true);
@@ -65,8 +59,9 @@ function Login() {
       <h1>เข้าสู่ระบบ</h1>
         <p>ลงชื่อเข้าใช้ด้วยบัญชีผู้ใช้ของคณะเทคโนโลยีสารสนเทศ
         <br/ >มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
-        <p><Button type="primary" htmlType="submit" onClick={mockloginstudent} loading={isLoading1}>ลงชื่อเข้าใช้ในฐานะนักเรียน</Button></p>  
+        <p><Button type="primary" htmlType="submit" size="middle" onClick={mockloginstudent} loading={isLoading1}>ลงชื่อเข้าใช้ในฐานะนักเรียน</Button></p>  
         <p><Button type="primary" htmlType="submit" onClick={mockloginteacher} loading={isLoading2}>ลงชื่อเข้าใช้ในฐานะอาจารย์ผู้สอน</Button></p>  
+        <p><Button type="primary" htmlType="submit" onClick={handlelogin} loading={isLoading2} style={{backgroundColor:"#fa4612"}}>ลงชื่อเข้าใช้ด้วย KMUTT Accounts</Button></p>  
       </div>
     </div>
   );
