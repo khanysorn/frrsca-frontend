423162fff7c0dcffbd67b965cd72534f859eec6e
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-26 13:23:10 +0700

    Redirect Path to Login

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,38 +1,47 @@
 import React, { useState } from "react";
-import {Button} from "antd";
+import {Button, Icon} from "antd";
 import firebase from "firebase";
 import app from '../config';
 import { useHistory } from "react-router-dom";
 import LoginBox from '../components/LoginBox'
+import KMUTTLogo from '../assets/image/kmutt.svg'
 
 
-const provider = new firebase.auth.OAuthProvider('microsoft.com');
 
 
 
 function Login() {
-  let history = useHistory();
+
+  const provider = new firebase.auth.OAuthProvider('microsoft.com');
+
+  const history = useHistory();
 
   const [isLoading1,setLoading1] = useState(false);
 
   const [isLoading2,setLoading2] = useState(false);
 
-  firebase.auth().signInWithPopup(provider)
-  .then(function(result) {
-    // User is signed in.
-    // IdP data available in result.additionalUserInfo.profile.
-    // OAuth access token can also be retrieved:
-    // result.credential.accessToken
-    // OAuth ID token can also be retrieved:
-    // result.credential.idToken
-    history.push("/student/class")
-  })
-  .catch(function(error) {
-    // Handle error.
-  });
+  const [isLoading3,setLoading3] = useState(false);
 
   function handlelogin(){
+    setLoading3(true);
+    setTimeout(()=>{
+      setLoading1(false);
+      history.push("/student/class")
+  },1000)
     app.auth().signInWithPopup(provider)
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      // User is signed in.
+      // IdP data available in result.additionalUserInfo.profile.
+      // OAuth access token can also be retrieved:
+      // result.credential.accessToken
+      // OAuth ID token can also be retrieved:
+      // result.credential.idToken
+      history.push("/student/class")
+    })
+    .catch(function(error) {
+      // Handle error.
+    });
   }
 
   function mockloginstudent(){
@@ -61,7 +70,7 @@ function Login() {
         <br/ >มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
         <p><Button type="primary" htmlType="submit" size="middle" onClick={mockloginstudent} loading={isLoading1}>ลงชื่อเข้าใช้ในฐานะนักเรียน</Button></p>  
         <p><Button type="primary" htmlType="submit" onClick={mockloginteacher} loading={isLoading2}>ลงชื่อเข้าใช้ในฐานะอาจารย์ผู้สอน</Button></p>  
-        <p><Button type="primary" htmlType="submit" onClick={handlelogin} loading={isLoading2} style={{backgroundColor:"#fa4612"}}>ลงชื่อเข้าใช้ด้วย KMUTT Accounts</Button></p>  
+        <p><Button type="primary" htmlType="submit" component={<KMUTTLogo />}onClick={handlelogin} loading={isLoading3} style={{backgroundColor:"#fa4612"}} danger>ลงชื่อเข้าใช้ด้วย KMUTT Accounts</Button></p>  
       </div>
     </div>
   );
