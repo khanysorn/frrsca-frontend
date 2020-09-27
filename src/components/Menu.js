7f9ac46fd2ead6b3fdfeac103a0583a2fa7c57f1
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-27 23:36:16 +0700

    [UPDATE] Student Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Menu } from "antd";
 import MenuBarStyle from './MenuBarStyle'
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MenuBar() {
     return(    
     <Menu theme="light" mode="horizontal" defaultSelectedKeys={["1"]} style={MenuBarStyle}>
-    <Menu.Item key="1">ห้องเรียนของฉัน</Menu.Item>
-    
+    <Menu.Item key="1"><Link to="/student/class">ห้องเรียนของฉัน</Link></Menu.Item>
     </Menu>)
   }
   
