81192caa27eeead4d2e09a755f936306d70f56ed
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-27 23:38:21 +0700

    [MOVED] Router in App.js to routes/index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './page/Home';
-import Login from './page/Login';
-import StudentClassList from './page/student/Class'
-import StudentClassDetail from './page/student/ClassDetail';
-import TeacherClassList from './page/teacher/Class'
-import TeacherClassDetail from './page/teacher/ClassDetail'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Routing from './routes';
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/student/class" component={StudentClassList} />
-      <Route path="/student/classdetail" component={StudentClassDetail} />
-      <Route path="/teacher/class" component={TeacherClassList} />
-      <Route path="/teacher/classdetail" component={TeacherClassDetail} />
-      </Switch>
-    </Router>
+    <Routing />
   );
 }
 
