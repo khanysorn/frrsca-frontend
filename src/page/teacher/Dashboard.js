b967954267e211becd7a5db069c1b2bbaa4a3de8
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-11-15 18:19:56 +0700

    [UPDATE] New Routing for Teacher Role 👨🏻‍🏫

---
+++ src/page/teacher/Dashboard.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Layout, Breadcrumb } from 'antd';
+import MenuBar from '../../components/teacher/Menu'
+const { Header, Content, Footer, Sider } = Layout;
+
+
+class ClassDetail extends React.Component {
+
+  // Jew witten
+  // onClickMenu = (id) => {
+  //   console.log(id)
+  // }
+  //   onclickmenu = (id) => {
+  //   this.props.idpage(id)
+  // }
+
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  handleSizeChange = e => {
+    this.setState({ size: e.target.value });
+  };
+
+
+
+  render() {
+    const { collapsed } = this.state;
+    
+    return (
+    <Layout style={{ minHeight: '100vh' }}>
+    <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} theme="light">
+      <div className="logo" />
+      <MenuBar  />
+    </Sider>
+    <Layout className="site-layout">
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '0 16px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>วิชาของคุณ</Breadcrumb.Item>
+          <Breadcrumb.Item>INT305</Breadcrumb.Item>
+          <Breadcrumb.Item>ภาพรวมของคุณ</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          Bill is a cat. 
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
+  );
+  }
+  }
+
+export default ClassDetail;
