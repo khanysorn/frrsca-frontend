5a5fabf6aabb374e631abe762530790025bbe977
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-11-15 23:41:21 +0700

    [UPDATE] 404 UI

--- src/page/404.js
+++ src/page/404.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Result, Button } from 'antd';
+import React from "react";
+import { Result, Button } from "antd";
+import ModalBox from '../components/ModalBox';
+import { Link } from "react-router-dom";
 
-function PageNotFound(){
-return(
-    <>
-    <Result
-    status="404"
-    title={<h2>ไม่พบหน้าที่คุณต้องการ</h2>}
-    subTitle="ขออภัย, ไม่พบหน้าที่คุณต้องการ"
-    extra={<Button type="primary">กลับไปยังหน้าแรก</Button>}
-  />
-  </>
-)
+function PageNotFound() {
+  return (
+    <div className="background">
+      <div style={ModalBox}>
+        <Result
+          status="warning"
+          title={<h2>ไม่พบหน้าที่คุณต้องการ</h2>}
+          subTitle="ขออภัย, ไม่พบหน้าที่คุณต้องการ"
+          extra={<Button type="primary"><Link to="/">กลับไปยังหน้าแรก</Link></Button>}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
