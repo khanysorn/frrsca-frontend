9c98c85f0494e978720251257726a5a6378f5266
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-11-16 00:31:26 +0700

    [UPDATE] Teacher Class Menu

--- src/components/teacher/Menu.js
+++ src/components/teacher/Menu.js
@@ -7,19 +7,19 @@ import {
   BookOutlined,
   SettingOutlined
 } from '@ant-design/icons';
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 const MenuBar = () => {
     const location = useLocation();
     const path = location.pathname.split("/").reverse()[0]
     return(    
       <Menu theme="light" defaultSelectedKeys={[path]} mode="inline">
-      <Button type="primary" shape="round" icon={<DashboardOutlined />} size={'normal'} style={{margin: '8px'}}>Real-time Dashboard</Button>
-        <Menu.Item key="summary" icon={<DashboardOutlined />} >ภาพรวมรายวิชา</Menu.Item>
-        <Menu.Item key="attendance" icon={<FormOutlined />} >การเช็กชื่อ</Menu.Item>
-        <Menu.Item key="list" icon={<UserOutlined />}>รายชื่อนักศึกษา</Menu.Item>
-        <Menu.Item key="report" icon={<BookOutlined />}>รายงาน</Menu.Item>
-        <Menu.Item key="setting" icon={<SettingOutlined />}>การตั้งค่า</Menu.Item>
+        <Button type="primary" shape="round" icon={<DashboardOutlined />} size={'normal'} style={{margin: '8px'}}>Real-time Dashboard</Button>
+        <Menu.Item key="summary" icon={<DashboardOutlined />}><Link to="/teacher/class/summary">ภาพรวมรายวิชา</Link></Menu.Item>
+        <Menu.Item key="attendance" icon={<FormOutlined />}><Link to="/teacher/class/student/attendance">การเช็กชื่อ</Link></Menu.Item>
+        <Menu.Item key="list" icon={<UserOutlined />}><Link to="/teacher/class/student/list">รายชื่อนักศึกษา</Link></Menu.Item>
+        <Menu.Item key="report" icon={<BookOutlined />}><Link to="/teacher/class/student/report">รายงาน</Link></Menu.Item>
+        <Menu.Item key="setting" icon={<SettingOutlined />}><Link to="/teacher/class/setting">การตั้งค่า</Link></Menu.Item>
       </Menu>)
 }
   
