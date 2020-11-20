922edb292a37774750862f28c76863c476a04096
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-11-21 02:28:29 +0700

    [REMOVE] Unused component

--- src/page/teacher/ReportByAttendance.js
+++ src/page/teacher/ReportByAttendance.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Breadcrumb, Avatar, Space, Table } from 'antd';
+import { Layout, Breadcrumb, Avatar, Table } from 'antd';
 import MenuBar from '../../components/teacher/Menu'
 import User from '../../components/User'
 import Footer from '../../components/Footer';
